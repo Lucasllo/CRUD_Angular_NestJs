348bdd0ed5a5c822b856ed573672e685854af211
Lucas Lopes <lucaslopesoliveira@gmail.com>
2024-11-11 15:09:55 -0300

    adição de usuarios e playlists na criação do banco

--- crud-api/src/services/user.service.ts
+++ crud-api/src/services/user.service.ts
@@ -24,7 +24,132 @@ export class UserService {
       password: "$2b$10$Nq.tY4hK4ACZItSHQKAyWuF0bQEBW5o4JDPpvXm4DUmYuSAMAKWs2", //Asdf@1234
       gender: "Masculino",
       agree: true,
-      playlists: [],
+      playlists: [{
+        id: 4,
+        category: "testando",
+        name: "nova tarefa usuario 2",
+        videos: [
+          {
+            id: 5,
+            name: "testar novamente",
+            youtubeUrl: "7hHZnvjCbVw",
+            playlistId: 4,
+          },
+        ],
+        userId: 1,
+      },
+      {
+        id: 5,
+        category: "testando",
+        name: "teste demo usuario",
+        videos: [
+          { id: 6, name: "demo", youtubeUrl: "m3lF2qEA2cw", playlistId: 5 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 6,
+        category: "testando",
+        name: "teste demo usuario 2",
+        videos: [
+          { id: 7, name: "demo 2", youtubeUrl: "OBmlCZTF4Xs", playlistId: 6 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 7,
+        category: "testando",
+        name: "tarefa demo usuario",
+        videos: [
+          { id: 8, name: "demo ", youtubeUrl: "7hHZnvjCbVw", playlistId: 7 },
+          { id: 9, name: "demo 2", youtubeUrl: "OBmlCZTF4Xs", playlistId: 7 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 11,
+        category: "testando",
+        name: "nova tarefa usuario",
+        videos: [
+          {
+            id: 14,
+            name: "testar novamente",
+            youtubeUrl: "7hHZnvjCbVw",
+            playlistId: 11,
+          },
+        ],
+        userId: 1,
+      },
+      {
+        id: 12,
+        category: "testando",
+        name: "teste demo usuario",
+        videos: [
+          { id: 15, name: "demo", youtubeUrl: "m3lF2qEA2cw", playlistId: 12 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 13,
+        category: "testando",
+        name: "teste demo usuario 3",
+        videos: [
+          { id: 16, name: "demo 2", youtubeUrl: "OBmlCZTF4Xs", playlistId: 13 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 14,
+        category: "testando",
+        name: "tarefa demo usuario",
+        videos: [
+          { id: 17, name: "demo ", youtubeUrl: "7hHZnvjCbVw", playlistId: 14 },
+          { id: 18, name: "demo 2", youtubeUrl: "OBmlCZTF4Xs", playlistId: 14 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 18,
+        category: "testando",
+        name: "nova tarefa usuario",
+        videos: [
+          {
+            id: 23,
+            name: "testar novamente",
+            youtubeUrl: "7hHZnvjCbVw",
+            playlistId: 18,
+          },
+        ],
+        userId: 1,
+      },
+      {
+        id: 19,
+        category: "testando",
+        name: "teste demo usuario",
+        videos: [
+          { id: 24, name: "demo", youtubeUrl: "m3lF2qEA2cw", playlistId: 19 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 20,
+        category: "testando",
+        name: "teste demo usuario 3",
+        videos: [
+          { id: 25, name: "demo 2", youtubeUrl: "OBmlCZTF4Xs", playlistId: 20 },
+        ],
+        userId: 1,
+      },
+      {
+        id: 21,
+        category: "testando",
+        name: "tarefa demo usuario",
+        videos: [
+          { id: 26, name: "demo ", youtubeUrl: "7hHZnvjCbVw", playlistId: 21 },
+          { id: 27, name: "demo 2", youtubeUrl: "OBmlCZTF4Xs", playlistId: 21 },
+        ],
+        userId: 1,
+      },],
       createdAt: this.dateWithoutTimezone(new Date()),
       updatedAt: this.dateWithoutTimezone(new Date()),
       validatedEmail: true,
@@ -39,7 +164,138 @@ export class UserService {
       password: "testee",
       gender: "Masculino",
       agree: true,
-      playlists: [],
+      playlists: [{
+        id: 1,
+        category: "testando",
+        name: "teste usuario",
+        videos: [
+          { id: 1, name: "testar", youtubeUrl: "OBmlCZTF4Xs", playlistId: 1 },
+        ],
+        userId: 2,
+      },
+      {
+        id: 2,
+        category: "testando",
+        name: "teste usuario 2",
+        videos: [
+          {
+            id: 2,
+            name: "testar novamente",
+            youtubeUrl: "7hHZnvjCbVw",
+            playlistId: 2,
+          },
+        ],
+        userId: 2,
+      },
+      {
+        id: 3,
+        category: "testando",
+        name: "nova tarefa usuario",
+        videos: [
+          { id: 3, name: "testar ", youtubeUrl: "m3lF2qEA2cw", playlistId: 3 },
+          { id: 4, name: "testar 2", youtubeUrl: "7hHZnvjCbVw", playlistId: 3 },
+        ],
+        userId: 2,
+      },
+      {
+        id: 8,
+        category: "testando",
+        name: "teste usuario 2",
+        videos: [
+          { id: 10, name: "testar", youtubeUrl: "OBmlCZTF4Xs", playlistId: 8 },
+        ],
+        userId: 2,
+      },
+      {
+        id: 9,
+        category: "testando",
+        name: "teste 2 usuario 2",
+        videos: [
+          {
+            id: 11,
+            name: "testar novamente",
+            youtubeUrl: "7hHZnvjCbVw",
+            playlistId: 9,
+          },
+        ],
+        userId: 2,
+      },
+      {
+        id: 10,
+        category: "testando",
+        name: "nova tarefa usuario 3",
+        videos: [
+          { id: 12, name: "testar ", youtubeUrl: "m3lF2qEA2cw", playlistId: 10 },
+          { id: 13, name: "testar 2", youtubeUrl: "7hHZnvjCbVw", playlistId: 10 },
+        ],
+        userId: 2,
+      },
+    {
+      id: 15,
+      category: "testando",
+      name: "teste usuario 3",
+      videos: [
+        { id: 19, name: "testar", youtubeUrl: "OBmlCZTF4Xs", playlistId: 15 },
+      ],
+      userId: 2,
+    },
+    {
+      id: 16,
+      category: "testando",
+      name: "teste 2 usuario 2",
+      videos: [
+        {
+          id: 20,
+          name: "testar novamente",
+          youtubeUrl: "7hHZnvjCbVw",
+          playlistId: 16,
+        },
+      ],
+      userId: 2,
+    },
+    {
+      id: 17,
+      category: "testando",
+      name: "nova tarefa usuario 4",
+      videos: [
+        { id: 21, name: "testar ", youtubeUrl: "m3lF2qEA2cw", playlistId: 17 },
+        { id: 22, name: "testar 2", youtubeUrl: "7hHZnvjCbVw", playlistId: 17 },
+      ],
+      userId: 2,
+    },
+    {
+      id: 15,
+      category: "testando",
+      name: "teste usuario 3",
+      videos: [
+        { id: 19, name: "testar", youtubeUrl: "OBmlCZTF4Xs", playlistId: 15 },
+      ],
+      userId: 2,
+    },
+    {
+      id: 16,
+      category: "testando",
+      name: "teste 2 usuario 2",
+      videos: [
+        {
+          id: 20,
+          name: "testar novamente",
+          youtubeUrl: "7hHZnvjCbVw",
+          playlistId: 16,
+        },
+      ],
+      userId: 2,
+    },
+    {
+      id: 17,
+      category: "testando",
+      name: "nova tarefa usuario 4",
+      videos: [
+        { id: 21, name: "testar ", youtubeUrl: "m3lF2qEA2cw", playlistId: 17 },
+        { id: 22, name: "testar 2", youtubeUrl: "7hHZnvjCbVw", playlistId: 17 },
+      ],
+      userId: 2,
+    },],
       createdAt: this.dateWithoutTimezone(new Date()),
       updatedAt: this.dateWithoutTimezone(new Date()),
       validatedEmail: true,
